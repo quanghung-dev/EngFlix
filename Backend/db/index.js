e2dbe31fd68f891bc9a56314b89190ffc3935a13
Hung Do <hungd4842@gmail.com>
2026-07-08 15:47:25 +0700

    replace local db with neon

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,12 +1,27 @@
 const {Pool} = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-});
+const poolConfig = {};
+
+if (process.env.DATABASE_URL) {
+    poolConfig.connectionString = process.env.DATABASE_URL;
+} else {
+    poolConfig.user = process.env.DB_USER;
+    poolConfig.password = process.env.DB_PASSWORD;
+    poolConfig.host = process.env.DB_HOST;
+    poolConfig.port = process.env.DB_PORT;
+    poolConfig.database = process.env.DB_DATABASE;
+}
+
+if (
+    (poolConfig.connectionString && (poolConfig.connectionString.includes('sslmode=require') || poolConfig.connectionString.includes('neon.tech'))) ||
+    (poolConfig.host && poolConfig.host.includes('neon.tech'))
+) {
+    poolConfig.ssl = {
+        rejectUnauthorized: false
+    };
+}
+
+const pool = new Pool(poolConfig);
 
 module.exports = pool;
